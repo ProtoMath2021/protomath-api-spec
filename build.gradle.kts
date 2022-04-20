b0a3f5f853e3e506e4fade262796f0678a66da84
muratslavich <slavichmo@gmail.com>
2022-04-20 03:13:59 +0300

    fix build jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,13 @@ plugins {
     `java-library`
 }
 
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+
 openApiGenerate {
     generatorName.set("spring")
     inputSpec.set("$rootDir/specs/api.yaml")
@@ -11,4 +18,18 @@ openApiGenerate {
     configFile.set("$rootDir/api-config.json")
     typeMappings.set(mapOf("integer" to "Long"))
     validateSpec.set(true)
+}
+
+sourceSets["main"].java.srcDir(file("$buildDir/generated/src/main/java"))
+
+tasks.compileJava { dependsOn("openApiGenerate") }
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.0.1")
+    implementation("javax.validation:validation-api:2.0.1.Final")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("io.swagger.core.v3:swagger-annotations:2.1.13")
+    implementation("org.springframework:spring-context:5.3.19")
+    implementation("org.springframework:spring-web:5.3.19")
+    compileOnly("javax.servlet:servlet-api:2.5")
 }
